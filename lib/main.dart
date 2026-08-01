424d341f5c162e1920cf8fbd2b47dd403043c8aa
Roland Levy <contact@rolandlevy.co.uk>
2026-08-01 23:33:32 +0100

    Create a Flutter sandbox app with counter functionality
    Implement a simple Flutter app with a counter feature.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,51 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(const SandboxApp());
+
+class SandboxApp extends StatelessWidget {
+  const SandboxApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Codespace Sandbox Test',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const CounterPage(),
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _count = 0;
+
+  void _increment() => setState(() => _count++);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Sandbox Test')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('If this works, the sandbox is good:'),
+            const SizedBox(height: 16),
+            Text('$_count', style: const TextStyle(fontSize: 48)),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _increment,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
